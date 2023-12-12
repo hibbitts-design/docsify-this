e9b6c792c4e4da298b33baede9f20b85d9c022a6
Paul Hibbitts <paul@paulhibbitts.com>
2023-12-12 12:24:30 -0800

    Update to latest Docsify-Wikilink Plugin script with support for WikiLinks in Tables.

--- docs/assets/vendor/docsify/plugins/docsify-wikilink.js
+++ docs/assets/vendor/docsify/plugins/docsify-wikilink.js
@@ -1,1 +1,57 @@
-!function(){"use strict";window.$docsify?window.$docsify.plugins=[].concat((function(n,e){n.beforeEach((function(n){return n})),n.afterEach((function(n,e){var i=document.createElement("div");i.innerHTML=n;var t=location.hash.lastIndexOf("/"),o=location.hash.substring(0,t+1);const r=[];r.push(...i.getElementsByTagName("p")),r.push(...i.getElementsByTagName("li"));for(var c=0;c<r.length;c++){const n=r[c].innerHTML.replace(/\[\[([^\[\]]+)\]\]/g,(function(n){const e=n.replace("[[","").replace("]]",""),i=e.split("|"),t=2===i.length?`${i[0].trim()}`:e;var r=t,c="",s=e;if(-1!=t.indexOf("#")){const n=t.split("#");r=n[0],c=`?id=${n[1]}`,s=`${i[1].trim()}`}return 2==s.split("|").length&&(s=s.split("|")[1]),0===r.indexOf("/")?`<a href="#${r}${c}">${s}</a>`:`<a href="${o}${r}${c}">${s}</a>`}));r[c].innerHTML=n}e(i.innerHTML)}))}),window.$docsify.plugins):console.error(" 这是一个 docsify 插件，请先引用 docsify 库！")}();
+'use strict';
+
+// Docsify plugin functions
+function plugin(hook, vm) {
+  hook.beforeEach(function (content) {
+    return content
+  })
+  hook.afterEach(function (html, next) {
+    // 解析成 html 后调用。
+    // beforeEach 和 afterEach 支持处理异步逻辑
+    var mdDom = document.createElement(
+      'div')
+    mdDom.innerHTML=html;
+    var index = location.hash.lastIndexOf('/')
+    var relativePath = location.hash.substring(0, index + 1)
+    const list = []
+    list.push(...mdDom.getElementsByTagName('p'))
+    list.push(...mdDom.getElementsByTagName('li'))
+    list.push(...mdDom.getElementsByTagName('td'))
+    list.push(...mdDom.getElementsByTagName('th'))
+    
+    for (var i=0; i < list.length; i++) {
+      var para = list[i].innerHTML
+      const eachParaRes = para.replace(/\[\[([^\[\]]+)\]\]/g, function (content) {
+        const innerContent = content.replace('[[', '').replace(']]', '')
+        const linkAliasSps = innerContent.split('|') // [link, alias]
+        // [[link|showtext]] = 创建一个链接，指向内部的wiki页面'Link', 链接文字是'click here'.
+        const link = linkAliasSps.length === 2 ? `${linkAliasSps[0].trim()}` : innerContent
+        var hashPath = link
+        var topic = ''
+        var showText = innerContent
+        if (link.indexOf('#') != -1) {
+          const linkTopicSps = link.split('#') // link, topic
+          hashPath = linkTopicSps[0]
+          topic = `?id=${linkTopicSps[1]}`
+          showText = `${linkAliasSps[1].trim()}`
+        }
+        if (showText.split("|").length==2){showText=showText.split("|")[1]}
+        if (hashPath.indexOf('/') === 0) {
+          //absolute path
+          return `<a href="#${hashPath}${topic}">${showText}</a>`
+        } else {
+          return `<a href="${relativePath}${hashPath}${topic}">${showText}</a>`
+        }
+      })
+      list[i].innerHTML = eachParaRes
+    }
+
+    next(mdDom.innerHTML)
+  })
+}
+
+if (!window.$docsify) {
+  console.error(' 这是一个 docsify 插件，请先引用 docsify 库！');
+} else {
+  window.$docsify.plugins = [].concat(plugin, window.$docsify.plugins);
+}
