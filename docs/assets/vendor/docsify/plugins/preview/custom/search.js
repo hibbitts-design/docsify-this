ccc6c4b3ab40dc0fb5434ba8b30a14ce88eda8da
Paul Hibbitts <paul@paulhibbitts.com>
2024-06-19 08:47:59 -0700

    Revert "Test updated Search filter for Markdown/HTML content."
    This reverts commit c8337525322add1d61007b76f9cd6e97a27fbf29.

--- docs/assets/vendor/docsify/plugins/preview/custom/search.js
+++ docs/assets/vendor/docsify/plugins/preview/custom/search.js
@@ -242,6 +242,14 @@
       const postContent = post.body && post.body.trim();
       const postUrl = post.slug || '';
 
+      // Skip posts that contain iframes, Font Awesome icons, embedly cards, or Markdown images
+      // console.log(postContent);
+      const isImage = /!\[[^\]]*\]\([^)]*\)/g.test(postContent); // Check if it's a Markdown image
+
+      if (postContent.includes('iframe') || postContent.includes(':fas') || postContent.includes(':fab') || postContent.includes('embedly-card') || isImage) {
+        continue;
+      }
+
       if (postTitle) {
         keywords.forEach(keyword => {
           // From https://github.com/sindresorhus/escape-string-regexp
@@ -257,15 +265,9 @@
           handlePostTitle = postTitle
             ? escapeHtml(ignoreDiacriticalMarks(postTitle))
             : postTitle;
-
-          // Skip content that contain iframes, Font Awesome icons, embedly cards, or Markdown images
-          // console.log(postContent);
-          const isImage = /!\[[^\]]*\]\([^)]*\)/g.test(postContent); // Check if it's a Markdown image
-          if (!(postContent.includes('iframe') || postContent.includes(':fas') || postContent.includes(':fab') || postContent.includes('embedly-card') || isImage)) {
-            handlePostContent = postContent
+          handlePostContent = postContent
             ? escapeHtml(ignoreDiacriticalMarks(postContent))
             : postContent;
-          }
 
           indexTitle = postTitle ? handlePostTitle.search(regEx) : -1;
           indexContent = postContent ? handlePostContent.search(regEx) : -1;
